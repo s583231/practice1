d7102052f300003e00e73a41d191f070ca132db4
suqiao <179284352@qq.com>
2019-07-01 18:35:26 +0800

    视频

--- src/components/video/index1.vue
+++ src/components/video/index1.vue
@@ -1,10 +1,20 @@
 <template>
   <!-- 视频 -->
   <div class="video" @click="btn">
-    <video src="http://www.zhuoyuekecheng.com/ilife/upload/apk/20190506160025608_lxilg.mp4" id="video"></video>
+    <video :src="url" id="video"></video>
     <!-- <video :src="url" id="video" loop='true'></video> -->
     <img v-if="isshow" src="../../assets/images/audio/audio-icon.png" alt="">
     <div class="control">
+      <div>
+        <div>进度条</div>
+      </div>
+      <div class="controlButton">
+        <div>暂停</div>
+        <div>声音</div>
+        <div>倍速</div>
+        <div>循环</div>
+        <div>桌面全屏</div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +26,8 @@ export default {
   },
   data () {
     return {
-      url: 'http://www.zhuoyuekecheng.com/ilife/upload/apk/20190506160025608_lxilg.mp4',
+      // url: 'http://www.zhuoyuekecheng.com/ilife/upload/apk/20190506160025608_lxilg.mp4',
+      url: 'http://www.zhuoyuekecheng.com/ilife/upload/apk/20190523165105852_s5r7i.avi',
       video: null,
       isshow: true
     }
@@ -40,21 +51,23 @@ export default {
 
 <style scoped>
 .video{
-  height: 50vh;
+  width: 900px;
+  height: 600px;
   position: relative;
   border:1px solid red;
 }
 video{
-  /* width:500px; */
-  height: 50vh;
+  width: 900px;
+  height: 600px;
+  border:1px solid green;
 }
 img{
   position: absolute;
   width:60px;
   height:60px;
   border-radius:50%;
-  top:200px;
-  right:360px;;
+  top:270px;
+  right:420px;;
 }
 .control{
   width:100%;
@@ -62,5 +75,13 @@ img{
   background: rgba(0, 0, 0, 0.3);
   position: absolute;
   bottom: 0;
+}
+.controlButton{
+  display: flex
+}
+.controlButton > div{
+  line-height: 40px;
+  margin-right: 5px;
+  color: #fff;
 }
 </style>
